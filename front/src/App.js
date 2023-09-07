66280ef1dbbbe0191f25e67e87f87a50f7f7bd04
sayyyho <323psh@naver.com>
2023-09-07 14:34:25 +0900

    Refactor: Header 컴포넌트화

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./components/Header";
+import LeftNav from "./components/info/LeftNav";
+import RightNav from "./components/info/RightNav";
+
+export function App() {
+  return (
+    <>
+      <Header />
+      <LeftNav />
+      <RightNav />
+    </>
+  );
+}
---
+++ front/src/components/info/RightNav/RightNav.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function RightNav({ user }) {
+  return (
+    <nav id="right-nav">
+      <div id="user">
+        {user ? (
+          // 로그인한 경우
+          <>
+            <div id="greeting">
+              <a href="#">Hello {user}!</a>
+            </div>
+            <div id="my-page" className="signup-mypage">
+              <a href={`./my_page/${user}`}>마이페이지</a>
+            </div>
+            <div id="logout" className="login-logout">
+              <a href="./logout">로그아웃</a>
+            </div>
+          </>
+        ) : (
+          // 로그인하지 않은 경우
+          <>
+            <div id="sign-up" className="signup-mypage">
+              <a>회원가입</a>
+            </div>
+            <div id="login" className="login-logout">
+              <a>로그인</a>
+            </div>
+          </>
+        )}
+      </div>
+      <div id="location">
+        <div>
+          <a>관심지역 설정</a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default RightNav;
