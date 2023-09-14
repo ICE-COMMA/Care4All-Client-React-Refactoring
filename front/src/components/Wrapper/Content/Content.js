290f2e5c0b8b34dc8fcd337f4adb998f24394f89
sayyyho <323psh@naver.com>
2023-09-14 23:18:45 +0900

    Feat: 슬라이드 기능 리팩토링

--- front/src/components/Wrapper/Content/Content.js
+++ front/src/components/Wrapper/Content/Content.js
@@ -1,56 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useMemo } from "react";
 import "./Content.css";
 import SpeechService from "../SpeechService/SpeechService";
 
 const Content = () => {
-  const dataSet = [
-    {
-      url: "https://data.seoul.go.kr/resources/img/guide/hotspot/구로디지털단지역.jpg",
-      text: ["구로디지털단지역", "혼잡"],
-      slide: ["●", "○", "○", "○"],
-    },
-    {
-      url: `https://www.smpa.go.kr/common/attachfile/attachfileView.do?attachNo=00232765&imgType=L`,
-      text: ["시위정보", null],
-      slide: ["○", "●", "○", "○"],
-    },
-    {
-      url: `https://media.istockphoto.com/id/1487359679/ko/벡터/직장에서-제복을-입은-전문-배관공-욕실-세면대를-고치는-배관공.jpg?s=1024x1024&w=is&k=20&c=MQs_FPXdMIm4YqBJ6zclGCWxRXXW3zmeF_fj6IamOro=`,
-      text: ["테스트 중", null],
-      slide: ["○", "○", "●", "○"],
-    },
-    {
-      url: `https://img.freepik.com/premium-vector/the-man-has-repaired-the-old-house-for-rent_701961-3369.jpg`,
-      text: ["테스트중", null],
-      slide: ["○", "○", "○", "●"],
-    },
-  ];
+  const dataSet = useMemo(
+    () => [
+      {
+        url: "https://data.seoul.go.kr/resources/img/guide/hotspot/구로디지털단지역.jpg",
+        text: ["구로디지털단지역", "혼잡"],
+        slide: ["●", "○", "○", "○"],
+      },
+      {
+        url: `https://www.smpa.go.kr/common/attachfile/attachfileView.do?attachNo=00232765&imgType=L`,
+        text: ["시위정보", null],
+        slide: ["○", "●", "○", "○"],
+      },
+      {
+        url: `https://media.istockphoto.com/id/1487359679/ko/벡터/직장에서-제복을-입은-전문-배관공-욕실-세면대를-고치는-배관공.jpg?s=1024x1024&w=is&k=20&c=MQs_FPXdMIm4YqBJ6zclGCWxRXXW3zmeF_fj6IamOro=`,
+        text: ["테스트 중", null],
+        slide: ["○", "○", "●", "○"],
+      },
+      {
+        url: `https://img.freepik.com/premium-vector/the-man-has-repaired-the-old-house-for-rent_701961-3369.jpg`,
+        text: ["테스트중", null],
+        slide: ["○", "○", "○", "●"],
+      },
+    ],
+    []
+  ); // 의존성 배열이 비어 있으므로 한 번만 초기화됩니다
+
   const [slideIndex, setSlideIndex] = useState(0);
   const [slideInfo, setSlideInfo] = useState(["○", "○", "○", "○"]);
   const [mainText, setMainText] = useState(null);
   const [subText, setSubText] = useState(null);
   const [contentUrl, setContentUrl] = useState(null);
 
-  useEffect(() => {
-    handleContent();
-  }, []);
-  // const len = contentInfo.length;
-  const handleContent = () => {
+  const handleContent = useCallback(() => {
     // 2초마다 slideIndex를 증가시켜 슬라이드 쇼를 진행합니다.
-    let currentIndex = 0;
-    const interval = setInterval(() => {
-      console.log(currentIndex);
-      setSlideIndex(currentIndex); // 슬라이드 인덱스 업데이트
-      setSlideInfo(dataSet[currentIndex].slide);
-      setContentUrl(dataSet[currentIndex].url);
-      setMainText(dataSet[currentIndex].text[0]);
-      setSubText(dataSet[currentIndex].text[1]);
-      currentIndex = (currentIndex + 1) % dataSet.length;
-    }, 2000);
+    setSlideInfo(dataSet[slideIndex].slide);
+    setContentUrl(dataSet[slideIndex].url);
+    setMainText(dataSet[slideIndex].text[0]);
+    setSubText(dataSet[slideIndex].text[1]);
+    setSlideIndex((prevSlideIndex) => (prevSlideIndex + 1) % dataSet.length);
+  }, [slideIndex, dataSet]);
 
-    // 컴포넌트가 언마운트될 때 인터벌을 정리합니다.
+  useEffect(() => {
+    const interval = setInterval(handleContent, 2000);
     return () => clearInterval(interval);
-  };
+  }, [handleContent]);
 
   return (
     <div className="content-box">
